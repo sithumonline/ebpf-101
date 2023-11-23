10a01bca4a68913f36b3fdc249724d7272469470
Sithum Bopitiya <sithumsandeepap@gmail.com>
2023-11-23 17:03:34 +0530

    net_dev ex

--- examples/net_dev_queue/trace_net.bpf.c
+++ examples/net_dev_queue/trace_net.bpf.c
@@ -1,4 +1,4 @@
-//go:build ignore
+// go:build ignore
 
 #include "vmlinux.h"
 #include "bpf/bpf_core_read.h"
@@ -58,6 +58,14 @@ get_ip(struct sk_buff *skb)
     return -1;
 }
 
+struct
+{
+    __uint(type, BPF_MAP_TYPE_LRU_HASH);
+    __uint(max_entries, 10240);
+    __type(key, __u32);
+    __type(value, __u32);
+} net_dev_queue SEC(".maps");
+
 SEC("tp/net/net_dev_queue")
 int handle_net_dev_queue(struct trace_event_raw_net_dev_template *ctx)
 {
@@ -67,6 +75,10 @@ int handle_net_dev_queue(struct trace_event_raw_net_dev_template *ctx)
     if (is_program(comm))
     {
         int res = get_ip((struct sk_buff *)ctx->skbaddr);
+
+        // print res
+        // bpf_printk("tp/net/net_dev_queue: %d", res);
+
         if (res != -1)
         {
             if (full_printk)
@@ -76,6 +88,18 @@ int handle_net_dev_queue(struct trace_event_raw_net_dev_template *ctx)
             else
             {
                 bpf_printk("tp/net/net_dev_queue: %d", res);
+            }
+
+            __u32 key = res;
+            __u32 *val = bpf_map_lookup_elem(&net_dev_queue, &key);
+            if (val)
+            {
+                __sync_fetch_and_add(val, 1);
+            }
+            else
+            {
+                __u32 initval = 1;
+                bpf_map_update_elem(&net_dev_queue, &key, &initval, BPF_ANY);
             }
         }
     }
---
+++ examples/netlink_extack/go.mod
@@ -0,0 +1,19 @@
+module gitlab.com/sithumonline/netlink_extack
+
+go 1.21.3
+
+require (
+	github.com/cilium/ebpf v0.12.3
+	github.com/florianl/go-tc v0.4.2
+	github.com/mdlayher/netlink v1.7.2
+	golang.org/x/sync v0.5.0
+	golang.org/x/sys v0.14.1-0.20231108175955-e4099bfacb8c
+)
+
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/socket v0.4.1 // indirect
+	golang.org/x/exp v0.0.0-20230224173230-c95f2b4c22f2 // indirect
+	golang.org/x/net v0.9.0 // indirect
+)
